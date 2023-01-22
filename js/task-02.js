e7381fcc5a8eeffdca750103577207a15e6100dc
Oleg <sirov@ukr.net>
2023-01-23 00:25:11 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,37 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elUl = document.querySelector('ul#ingredients');  
+
+const elLiMarkup = item => {
+
+  const elLi = document.createElement("li");
+
+  elLi.textContent = item;
+  elLi.classList.add("item");
+  console.log(elLi); 
+  return elLi;  
+
+}
+//  const elUlMarkup = ingredients.forEach(elLiMarkup);
+// const elUlMarkup = ingredients.forEach(function (item) {
+//     const elLi = document.createElement("li");
+//     elLi.textContent = item;
+//   elLi.classList.add("item");
+//   console.log(elLi); 
+//   return elLi;  
+// });
+
+
+const elUlMarkup = ingredients
+ .map(elLiMarkup)
+  .join(''); 
+   console.log(elUlMarkup); 
+
+
+
+elUl.insertAdjacentHTML('beforeend', elUlMarkup); 
+ 
+
+
