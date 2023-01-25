228796007b8c582605b379bda9075c88262a0511
Oleg <sirov@ukr.net>
2023-01-26 00:00:15 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені!");
+  }
+    const elements = {
+        email: email.value,
+        password: password.value, 
+    }
+  console.log(elements);
+  event.currentTarget.reset();
+}
