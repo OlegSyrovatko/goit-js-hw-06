293af54abfe1f1ef3fb61678917840ff50bb5d39
Oleg <sirov@ukr.net>
2023-01-27 08:36:42 +0200

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const els = {
+    inputEl: document.querySelector('input.number'),
+    createButton: document.querySelector('[data-action="create"]'),
+    destroyButton: document.querySelector('[data-action="destroy"]'),
+};
+
+
+els.createButton.addEventListener('click', function () { 
+
+  console.log(els.inputEl.innerHTML);
+
+});
+
+console.log(InputEl);
